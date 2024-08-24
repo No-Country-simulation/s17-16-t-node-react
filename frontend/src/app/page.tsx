1559f7cb6f0bce225c7888102270e8a22b2f7b51
Favianl <leofavianf@gmail.com>
2024-08-23 19:13:58 -0500

    initial frontend structure

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+export default function Home() {
+  return (
+    <main>
+      <h1>test</h1>
+      <div className='max-w-[200px]'>
+        <Input type='email' placeholder='email' />
+      </div>
+      <Button>click</Button>
+      <Button variant='outline'>click</Button>
+    </main>
+  );
+}
