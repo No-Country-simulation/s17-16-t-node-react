8bcfe6523a4952bbdaa15e0abda8ab299f25fb46
David Caycedo <69812733+DcDevRep@users.noreply.github.com>
2024-09-04 21:45:24 -0500

    feat: :construction: add cloudinary function and refactor user entity and crud

--- backend/src/api/users/index.js
+++ backend/src/api/users/index.js
@@ -2,6 +2,6 @@ export * from "./controller/user.controller.js";
 //export * from "./dto/user.dto.js";
 export * from "./dao/user.dao.js";
 export * from "./dao/model/user.model.js";
-export * from "./router/user.router.js";
+export * from "./router/user.routes.js";
 export * from "./services/user.services.js";
 export * from "./zod/user.zod.js";
